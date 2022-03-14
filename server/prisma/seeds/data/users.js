24e24ef8d635b45948a3ac83b2e05f4c7d147d9e
radouanefadel <radouane.ai@gmail.com>
2022-03-14 07:38:20 +0100

    :pencil2:db seeding: Replace "for loop" with "map"

--- server/prisma/seeds/data/users.js
+++ server/prisma/seeds/data/users.js
@@ -2,18 +2,16 @@ const Faker = require('@faker-js/faker');
 
 const faker = Faker.faker;
 
-let Users = [];
-
 // Generate 11 users with fake data
 // 10 users are authors, 1 is admin
-for (let i = 0; i < 11; i++) {
-	Users.push({
+const Users = Array(11).fill()
+	.map((value, index) => ({
 		firstName: faker.name.firstName(),
 		lastName: faker.name.lastName(),
 		email: faker.internet.email(),
 		password: faker.internet.password(),
-		role: i < 10 ? 'AUTHOR' : 'ADMIN',
-	});
-}
+		role: index < 10 ? 'AUTHOR' : 'ADMIN',
+	})
+);
 
 module.exports = Users;
