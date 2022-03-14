9c7b6a6245f119e017b9f031a3edde71c726bc79
radouanefadel <radouane.ai@gmail.com>
2022-03-14 19:36:44 +0100

    db seeding: Generate fake email from firstName & lastName

--- server/prisma/seeds/data/users.js
+++ server/prisma/seeds/data/users.js
@@ -5,13 +5,17 @@ const faker = Faker.faker;
 // Generate 11 users with fake data
 // 10 users are authors, 1 is admin
 const Users = Array(11).fill()
-	.map((value, index) => ({
-		firstName: faker.name.firstName(),
-		lastName: faker.name.lastName(),
-		email: faker.internet.email(),
-		password: faker.internet.password(),
-		role: index < 10 ? 'AUTHOR' : 'ADMIN',
-	})
+	.map((value, index) => {
+		const firstName = faker.name.firstName();
+		const lastName = faker.name.lastName();
+		return {
+			firstName,
+			lastName,
+			email: faker.internet.email(firstName, lastName).toLowerCase(),
+			password: faker.internet.password(),
+			role: index < 10 ? 'AUTHOR' : 'ADMIN',
+		};
+	}
 );
 
 module.exports = Users;
