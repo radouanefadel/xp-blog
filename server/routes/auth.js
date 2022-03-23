8aae7d14e9ff29e3fa13dc6bb663b8b7d0e768a6
radouanefadel <radouane.ai@gmail.com>
2022-03-23 07:46:07 +0100

    :sparkles:(feat) User registration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
 const express = require('express');
 const prismaClient = require('@prisma/client');
+
 const { issueJwt } = require('../middleware/utils');
+const { ucFirst } = require('../utils/strings');
 
 const router = express.Router();
 const prisma = new prismaClient.PrismaClient();
@@ -29,5 +32,48 @@ router.post('/login', async (req, res) => {
 		});
 	});
 });
+
+router.post('/register',
+	body('email').trim().isEmail(),
+	body('password').isLength({ min: 8 }),
+	body('firstName').trim().isLength({ min: 2 }),
+	body('lastName').trim().isLength({ min: 2 }),
+	async (req, res) => {
+		const { firstName, lastName, email, password } = req.body;
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array(),
+			});
+		}
+		const user = await prisma.user.findUnique({
+			where: { email },
+		});
+		if (!!user) {
+			res.status(409).json({
+				error: 'Email is already taken!',
+			});
+		} else {
+			await prisma.user.create({
+				data: {
+					firstName: ucFirst(firstName),
+					lastName: lastName.toUpperCase(),
+					email,
+					password: bcrypt.hashSync(password, 10),
+					role: 'AUTHOR',
+				},		
+			}).then(user => res.status(201).json({
+				message: 'User created successfully!',
+				user: {
+					name: `${user.firstName} ${user.lastName}`,
+					email: user.email,
+					role: user.role,
+				},
+			})).catch(err =>
+				res.status(500).json({ error: 'Unable to create user' })
+			);
+		}
+	}
+);
 
 module.exports = router;
---
+++ server/utils/strings.js
@@ -0,0 +1,8 @@
+const ucFirst = (str) => {
+	if (!str) return '';
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+module.exports = {
+	ucFirst,
+};
